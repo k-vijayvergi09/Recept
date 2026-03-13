a387203eebf2fd19e560632a8214387862b82401
k-vijayvergi09 <k.vijayvergi09@gmail.com>
2026-03-13 18:36:48 +0530

    fix: Text color did not change on dark mode toggle

--- app/src/main/java/com/samsung/android/recept/ui/theme/Theme.kt
+++ app/src/main/java/com/samsung/android/recept/ui/theme/Theme.kt
@@ -1,18 +1,24 @@
 package com.samsung.android.recept.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 import com.samsung.android.recept.ui.theme.colors.DarkOnboardingGradientColors
 import com.samsung.android.recept.ui.theme.colors.DarkOnboardingGradientStops
 import com.samsung.android.recept.ui.theme.colors.LightOnboardingGradientColors
@@ -60,7 +66,7 @@ private val DarkColorScheme = darkColorScheme(
 )
 private val LightColorScheme = lightColorScheme(
     primary             = VioletCore,
-    onPrimary           = Color(0x00000000),
+    onPrimary           = Color(0xFFFFFFFF),
     primaryContainer    = Light_UserBubbleBg,
     onPrimaryContainer  = VioletDeep,
     secondary           = GreenSuccess,
@@ -88,7 +94,8 @@ fun ReceptTheme(
 ) {
     val colorScheme = when {
         dynamicColor -> {
-            if (darkTheme) DarkColorScheme else LightColorScheme
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
@@ -100,8 +107,9 @@ fun ReceptTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = AppTypography,
-        content = {
+        typography = AppTypography
+    ) {
+        CompositionLocalProvider(LocalContentColor provides colorScheme.onBackground) {
             Box(modifier = Modifier.fillMaxSize().drawBehind {
                 val brush = tilted175Brush(gradientColors, gradientStops, size.width, size.height)
                 drawRect(brush = brush)
@@ -109,5 +117,5 @@ fun ReceptTheme(
                 content()
             }
         }
-    )
+    }
 }
